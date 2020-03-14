a1de5ba27e1aa68533147a25e066fb9ad6e45067
Amir Ayoub <techamir25@gmail.com>
2020-03-14 00:23:37 -0400

    Updated STATIC_ROOT

--- Blog_project/settings.py
+++ Blog_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-MEDIA_STATIC = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
 
  # media root is going to be the root where the uploaded directory where the files are going to be saved 
