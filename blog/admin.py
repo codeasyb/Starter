6f36d3e5e9c8d56f51c975b5ecd7a35ee15049e3
Amir Ayoub <techamir25@gmail.com>
2020-03-15 01:21:11 -0400

    Updated models.py, admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'status', 'created_on')
+	list_filter = ("status",)
+	search_fileds = ['title', 'content']
+	prepopulated_fields = {'author': ('title',)}
+
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,11 @@ class Post(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now) # we do not want to exectue the now by [ now() ]
 	author = models.ForeignKey (User, on_delete=models.CASCADE) # if the user is deleted then the post will be deleted
+	created_on = models.DateTimeField(auto_now=True)
+	status = models.IntegerField(choices = STATUS, default=0)
+
+	class Meta:
+		ordering = ['-created_on']
 
 	def __str__(self):
 		return self.title
